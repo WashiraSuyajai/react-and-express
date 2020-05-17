ee1b077cfaae4a03aa51caacba39ded86a46d2aa
WashiraSuyajai <washira.suyajai@gmail.com>
2020-05-17 22:28:12 +0700

    First commit

---
+++ client/src/pages/List.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class List extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+  }
+
+  componentDidMount() {
+      this.getList()
+  }
+
+  getList = () => {
+    fetch("/api/getList")
+      .then((res) => res.json())
+      .then((list) => this.setState({ list }));
+  };
+
+  render() {
+    const { list } = this.state;
+    return (
+      <div>
+        <h1>List of Items</h1>
+        {list.length ? (
+          <div>
+            {list.map((item) => {
+              return <div>{item}</div>;
+            })}
+          </div>
+        ) : (
+          <div>
+            <h2>No list item found</h2>
+          </div>
+        )}
+        {/* The Refresh button to home page */}
+        <Link to={"/"}>
+          <button variant="raised">Refresh</button>
+        </Link>
+      </div>
+    );
+  }
+}
+export default List
